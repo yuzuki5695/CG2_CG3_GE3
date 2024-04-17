bbe89587ccf5d9ed3f75d5ec1b57ba1312c5b1a4
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-04-18 00:49:38 +0900

    00_03_確認課題

--- main.cpp
+++ main.cpp
@@ -1,104 +1,53 @@
-#include <Windows.h>
-#pragma comment(lib,"d3d12.lib")
-#pragma comment(lib,"dxgi.lib")
-#include <cstdint>
-#include <string>
-#include <format>
-#include <d3d12.h>
-#include <dxgi1_6.h>
-#include <cassert>
-
-//クライアント領域のサイズ
-const uint32_t kClientWidth = 1280;
-const uint32_t kClientHeight = 720;
-
-void Log(const std::string& message)
-{
-	OutputDebugStringA(message.c_str());
-}
-
-std::wstring ConvertString(const std::string& str)
-{
-	if (str.empty())
-	{
-		return std::wstring();
-	}
-
-	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
-	if (sizeNeeded == 0)
-	{
-		return std::wstring();
-	}
-	//string -> wstring
-	std::wstring result(sizeNeeded, 0);
-	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
-	return result;
-}
-
-std::string ConvertString(const std::wstring& str)
-{
-	if (str.empty())
-	{
-		return std::string();
-	}
-
-	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
-	if (sizeNeeded == 0)
-	{
-		return std::string();
-	}
-	//wstring -> string
-	std::string result(sizeNeeded, 0);
-	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
-	return result;
-}
+#include<Windows.h>
+#include<cstdint>
 
 //ウィンドウプロシージャ
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-{
-	//メッセージに応じたゲーム固有の処理
-	switch (msg){
-		//ウィンドウ破棄
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
+	WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+		//ウィンドウが破棄された
 	case WM_DESTROY:
 		//OSに対して、アプリの終了を伝える
 		PostQuitMessage(0);
 		return 0;
 	}
-
-	//標準のメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-//Windowsアプリでのエントリーポイント（main関数）
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
-{
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+
 	WNDCLASS wc{};
 
-	//ウィンドウプロシージャ
+	//ウィンドウプロージャ
 	wc.lpfnWndProc = WindowProc;
-
-	//ウィンドウクラス名（なんでもいい）
+	//ウィンドウクラス名（なんでも良い）
 	wc.lpszClassName = L"CG2WindowClass";
-
-	//インスタンスハンドル
+	//インスタントハンドル
 	wc.hInstance = GetModuleHandle(nullptr);
-
 	//カーソル
 	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
-
 	//ウィンドウクラス登録
 	RegisterClass(&wc);
 
-	//ウィンドウサイズを表す構造体にクライアント領域を入れる
-	RECT wrc = { 0, 0, kClientWidth, kClientHeight };
 
-	//クライアント領域を元に実際のサイズにwrcにする
+	//クライアント領域のサイズ
+	const int32_t KClientWidth = 1280;
+	const int32_t KClientHeight = 720;
+
+	//ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0,0,KClientWidth,KClientHeight };
+
+	//クライアント領域を元の実際のサイズにwrcを変更してもらう
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-	//ウィンドウの生成
+
+	//ウィンドウ生成
 	HWND hwnd = CreateWindow(
-		wc.lpszClassName,	
-		L"CG2",	
+		wc.lpszClassName,
+		L"CG2",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
@@ -107,88 +56,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		nullptr,
 		nullptr,
 		wc.hInstance,
-		nullptr
-	);
+		nullptr);
 
-	//ウィンドウを表示
+	//ウィンドウ表示
 	ShowWindow(hwnd, SW_SHOW);
-
-	//DXGIファクトリーの生成
-	IDXGIFactory7* dxgiFactory = nullptr;
-
-	//関数が成功したかどうかをSUCCEEDEDマクロ判定できる
-	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
-
-	//初期化の根本的な部分でエラーが出た場合,プログラムが間違っているか、どうにもできない場合があるのでassertする
-	assert(SUCCEEDED(hr));
-
-	//使用するアダプタ用の変数。最初にnullptrを入れておく
-	IDXGIAdapter4* useAdapter = nullptr;
-
-	//アダプタを良い順に頼む
-	for (UINT i = 0;
-		dxgiFactory->EnumAdapterByGpuPreference(i,
-			DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND, ++i;){
-		//アダプターの情報を取得
-		DXGI_ADAPTER_DESC3 adapterDesc{};
-		hr = useAdapter->GetDesc3(&adapterDesc);
-		assert(SUCCEEDED(hr));	//取得できないのは一大事
-
-		//ソフトウェアアダプタでなければ採用!
-		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE))
-		{
-			//採用したアダプタの情報をログに出力。wstringの法なので注意
-			Log(ConvertString(std::format(L"Use Adapater\n:{}\n", adapterDesc.Description)));
-			break;
-		}
-		useAdapter = nullptr;	//ソフトウェアアダプタの場合は見なかったことにする
-	}
-
-	//適切なアダプタが見つからなかったので起動できない
-	assert(useAdapter != nullptr);
-
-	ID3D12Device* device = nullptr;
-
-	//機能レベルとログ出力用の文字列
-	D3D_FEATURE_LEVEL featureLevels[] ={
-		D3D_FEATURE_LEVEL_12_2,
-		D3D_FEATURE_LEVEL_12_1,
-		D3D_FEATURE_LEVEL_12_0
-	};
-
-	const char* featureLevelStrings[] = { "12.2","12.1","12.0" };
-
-	//高い順に生成できるか試す
-	for (size_t i = 0; i < _countof(featureLevels); i++){
-		//採用したアダプターでデバイスを生成
-		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
-
-		//指定した機能レベルでデバイスが生成できたか確認
-		if (SUCCEEDED(hr))
-		{
-			//生成できたのでログ出力を行いループを抜ける
-			Log(std::format("FeatureLevel : {}\n", featureLevelStrings[i]));
-			break;
-		}
-	}
-
-	//デバイスの生成がうまくいかなかったので起動できない
-	assert(device != nullptr);
-	Log("Complete create D3D12Device!!!\n");	//初期化完了のログを出す
 
 
 	MSG msg{};
-	//ウィンドウのｘボタンが押されるまでループ
-	while (msg.message != WM_QUIT){
-		//Windowにメッセージが来たら最優先で処理させる
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)){
+	//ウィンドウの×ボタンが押されるまでループ
+	while (msg.message != WM_QUIT) {
+		//windowもメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-		else{
-			//ゲームの処理
-
+		else {
+			//ゲーム処理
 		}
 	}
+
+
 	return 0;
 }
