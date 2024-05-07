f36ef93285e0e5eeaaf015562c8a05a1748c9145
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-05-08 04:50:20 +0900

    a

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,15 +1,15 @@
 struct VertexShaderOutput{
-float32_t4 Position : SV_POSILION;
+float32_t4 Position : SV_POSITION;
 };
 
 struct VertexShaderInput{
-float32_t4 Position : POSILION0;
+float32_t4 Position : POSITION0;
 };
 
 
-VertexShaderOutput main(VertexShaderInput inPut){
+VertexShaderOutput main(VertexShaderInput input){
 VertexShaderOutput output;
-output.Position =input.Position;
+output.Position =input.position;
 return output;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include <Windows.h>
+#include <Windows.h>
 #include <cstdint>
 #include <string>
 #include <format>
