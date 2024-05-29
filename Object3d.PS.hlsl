377620d0c90ce9c313b551241b32ca5643c7ab84
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-05-29 13:05:24 +0900

    CG2_02_02

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,63 +1,20 @@
+typedef float4 float32_t4;
 
-
-
-struct TransfomationMatrix {
-
-	float32_t4x4 MVP;
-
+struct Material
+{
+    float32_t4 color;
 };
 
+ConstantBuffer<Material> gMaterial : register(b0);
 
-ConstantBuffer<TransfomationMatrix> gtransformarionMatrix : registere(bo);
-
-struct VertexShaderOutput{
-float32_t4 position : SV_POSITION;
-}
-
-
-
-struct VertexShaderInput{
-float32_t4 position : SV_POSITION;
-}
-
-VertexShaderOutput main(VertexShaderInput niput){
-
-
-VertexShaderOutput output;
-
-output.position =mul(input.position,ggtransformarionMatrix.MVP);
-
-returbn potput;
-
+struct PixeShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
 };
 
-
-
-
-struct PixelShaderOutput{
-
-float32_t4 color : SV_TARGETO;
-
-};
-PixelShaderOutput main(){
-PixelShaderOutput output;
-output.color = float32_t4(1.0,1.0,1.0,1.0);
-return output;
-
-}
-
-struct Material{
-float32_t4 color;
-};
-
-ConstantBuffer<Naterial>gMaterial : register(b0);
-
-struct PixeShaderOutput{
- float32_t4 color : SV_TARGET0;
-};
-
-PixeShaderOutput main(){
-PixeShaderOutput output;
-output.color = gMaterial.color;
-return output; 
+PixeShaderOutput main()
+{
+    PixeShaderOutput output;
+    output.color = gMaterial.color;
+    return output;
 }
