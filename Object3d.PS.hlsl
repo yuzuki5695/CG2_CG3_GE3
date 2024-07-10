c94c0ea77bf1bfbf986394de2d28c83529a16513
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-07-10 14:45:17 +0900

    CG2_06_01

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -29,18 +29,20 @@ SamplerState gSampler : register(s0);
 PixeShaderOutput main(VertexShaderOutput input)
 {
     //TextureをSampling
-    float4 TransformesUV = mul(float4(input.texcoord,0.0f,1.0f),gMaterial.uvTransform);
+    float4 TransformesUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float4 textureColor = gTexture.Sample(gSampler, TransformesUV.xy);
     
     PixeShaderOutput output;
     
-    if (gMaterial.endbleLighting != 0){ // Linhthingする場合
+    if (gMaterial.endbleLighting != 0)
+    { // Linhthingする場合
         // half lambert
         float Ndont = dot(normalize(input.normal), -gDirectionalLight.direction);
         float cos = pow(Ndont * 0.5f + 0.5f, 2.0f);
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     }
-    else{ // Linhthingしない場合、前回までと同じ演算
+    else
+    { // Linhthingしない場合、前回までと同じ演算
         output.color = gMaterial.color * textureColor;
     }
     return output;
