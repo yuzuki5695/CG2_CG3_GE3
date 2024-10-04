63f29205edad9b85b9be0f6167d35335367de3ca
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-10-04 13:04:04 +0900

    02_01

---
+++ Input.cpp
@@ -0,0 +1,25 @@
+#include "Input.h"
+#include<cassert>
+#include<wrl.h>
+#define DIRECTINPUT_VERSION   0x0800 // DirectInputのバージョン指定
+#pragma comment(lib,"dinput8.lid")
+#pragma comment(lib,"dxguid.lid")
+
+using namespace Microsoft::WRL;
+
+void Input::Initialize() 
+{
+	HRESULT result;
+
+	// DirectInputのインスタンス生成
+
+
+
+
+}
+
+
+void Input::Update() 
+{
+
+}
---
+++ Input.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<Windows.h>
+
+// 入力
+class Input
+{
+public: // メンバ関数
+	// 初期化
+	void Initialize();
+	// 更新
+	void Update();
+
+
+
+};
+
