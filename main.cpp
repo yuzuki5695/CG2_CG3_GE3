064a0413001c2f27e2f75935cc49bb5ffa6d5d3c
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-12-21 23:01:15 +0900

    GE3_05_01_完了

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 #include "DirectXCommon.h"
 #include "Sprite.h"
 #include "SpriteCommon.h"
+#include"D3DResourceLeakChecker.h"
 #include"externals/imgui/imgui.h"
 #include"externals/imgui/imgui_impl_dx12.h"
 #include"externals/imgui/imgui_impl_win32.h"
