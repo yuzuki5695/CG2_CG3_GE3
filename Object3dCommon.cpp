b07018ed6bd9a1372ac4cebed188e44a9bb2fb45
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2024-12-23 12:12:48 +0900

    GE3_06_01_完了

--- Object3dCommon.cpp
+++ Object3dCommon.cpp
@@ -1,6 +1,25 @@
 #include "Object3dCommon.h"
 
-void Object3dCommon::Initialize() {
+void Object3dCommon::Initialize(DirectXCommon* dxCommon) {
+    // NULL検出
+    assert(dxCommon);
+    // 引数を受け取ってメンバ変数に記録する
+    dxCommon_ = dxCommon;
+    // グラフィックスパイプラインの生成
+    GraphicsPipelineGenerate();
+}
+
+void Object3dCommon::RootSignatureGenerate() {
+
+
+
+
+
+
+}
+
+void Object3dCommon::GraphicsPipelineGenerate() {
+    RootSignatureGenerate();
 
 
 
--- main.cpp
+++ main.cpp
@@ -259,7 +259,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
     // 3Dオブジェクト共通部の初期化
     object3dCommon = new Object3dCommon;
-    object3dCommon->Initialize();
+    object3dCommon->Initialize(dxCommon);
 
 #pragma endregion 基盤システムの初期化
 
