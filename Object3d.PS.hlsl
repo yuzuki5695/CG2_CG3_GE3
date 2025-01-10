825aeaabef0cde86804cbab0d4740f950925b6a9
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-01-10 11:56:43 +0900

    CG3_02_01_完了

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -66,10 +66,14 @@ PixeShaderOutput main(VertexShaderOutput input)
         float cos = pow(Ndont * 0.5f + 0.5f, 2.0f);
         float3 toEve = normalize(gCamera.worldPosition - input.worldPosition);
         
-        float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
-        float RdotE = dot(reflectLight, toEve);
-        float specularPow = pow(saturate(RdotE), gMaterial.shininess); // 反射強度
+        //float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
+        //float RdotE = dot(reflectLight, toEve);
+        //float specularPow = pow(saturate(RdotE), gMaterial.shininess); // 反射強度
         
+        float3 halfVector = normalize(-gDirectionalLight.direction + toEve);
+        float NDotH = dot(normalize(input.normal), halfVector);
+        float specularPow = pow(saturate(NDotH), gMaterial.shininess); // 反射強度
+         
         // 拡散反射
         float3 diffuse =
         gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
