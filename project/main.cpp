66a8096b93098e1c8b862a8631dbc1d316ba5de8
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-02-11 23:24:48 +0900

    PG3_評価課題_完了

--- project/main.cpp
+++ project/main.cpp
@@ -195,7 +195,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         ImGui::NewFrame();
 
         // 開発用UIの処理。実際に開発用のUIを出す場合はここをゲーム固有の処理に置き換える
-        ImGui::ShowDemoWindow();
+        //ImGui::ShowDemoWindow();
 
         ImGui::Begin("Sprite");
         ImGui::DragInt("Dise",&dice);
