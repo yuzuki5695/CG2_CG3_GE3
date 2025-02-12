57c91ff11dc88e869890677e728144482f0616e0
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-02-12 15:20:36 +0900

    GE3_06_03_完了

--- Object3d.h
+++ Object3d.h
@@ -1,9 +1,97 @@
 #pragma once
+#include"Transform.h"
+#include"Vector2.h"
+#include"Vector3.h"
+#include"Vector4.h"
+#include "Matrix4x4.h"
+#include<d3d12.h>
+#include<wrl.h>
+#include<cstdint>
+#include<fstream>
+#include<string>
+#include<vector>
+
+class Object3dCommon;
 
 //  3Dオブジェクト
 class Object3d
 {
 public: // メンバ関数
+	// 頂点データ
+	struct VertexData
+	{
+		Vector4 position;
+		Vector2 texcoord;
+		Vector3 normal;
+	};
+	// マテリアルデータ
+	struct Material {
+		Vector4 color;
+		int32_t endbleLighting;
+		float padding[3];
+		Matrix4x4 uvTransform;
+	};
+	// 座標変換行列データ
+	struct TransformationMatrix {
+		Matrix4x4 WVP;
+		Matrix4x4 World;
+	};
+	// 平行光源データ
+	struct DirectionalLight {
+		Vector4 color; //!< ライトの色
+		Vector3 direction; //!< ライトの向き
+		float intensity; //!< 輝度
+	};
+	struct MaterialDate {
+		std::string textureFilePath;
+		uint32_t textureindex = 0;
+	};
+	struct ModelDate {
+		std::vector<VertexData> vertices;
+		MaterialDate material;
+	};
+public: // メンバ関数
 	// 初期化
-	void Initialize();
+	void Initialize(Object3dCommon* object3dCommon);
+	// 更新処理
+	void Update();
+	// 描画処理
+	void Draw();
+
+	// .mtlファイルの読み取り
+	static Object3d::MaterialDate LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename);
+	// .objファイルの読み取り
+	static Object3d::ModelDate LoadObjFile(const std::string& directoryPath, const std::string& filename);
+
+private:
+	// 頂点データ作成
+	void VertexDatacreation();
+	// リソース
+	// マテリアル
+	void MaterialGenerate();
+	// トランスフォームマトリックス
+	void TransformationMatrixGenerate();
+	// ライディング
+	void DirectionalLightGenerate();
+private:
+	// ポインタ
+	Object3dCommon* object3dCommon = nullptr;
+	// Objファイルのデータ
+	ModelDate modelDate;
+	// バッファリソース
+	Microsoft::WRL::ComPtr <ID3D12Resource> vertexResoruce;
+	Microsoft::WRL::ComPtr <ID3D12Resource> materialResource;
+	Microsoft::WRL::ComPtr <ID3D12Resource> transformationMatrixResource;
+	Microsoft::WRL::ComPtr <ID3D12Resource> directionalLightResource;
+	// バッファリソース内のデータを指すポインタ
+	VertexData* vertexData = nullptr;
+	Material* materialData = nullptr;
+	TransformationMatrix* transformationMatrixData = nullptr;
+	DirectionalLight* directionalLightDate = nullptr;
+	// バッファリソースの使い道を補足するバッファビュー
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+
+	Transform transform;
+	Transform  cameraTransform;
+
 };
