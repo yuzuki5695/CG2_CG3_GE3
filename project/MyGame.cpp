380534ab2215ccbcb339c432c051d83d4cea69b9
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-02-17 15:50:44 +0900

    GE3_11_02_完了

--- project/MyGame.cpp
+++ project/MyGame.cpp
@@ -5,7 +5,99 @@ void MyGame::Finalize() {
 }
 
 void MyGame::Initialize() {
+    OutputDebugStringA("Hello,Directx!\n");
 
+    // ウィンドウ作成
+    // WindowsAPIの初期化
+    winApp = new WinApp();
+    winApp->Initialize();
+    // DirectXの初期化
+    dxCommon = new DirectXCommon();
+    dxCommon->Initialize(winApp);
+    // 音声読み込み
+    soundLoader = new SoundLoader();
+    soundLoader->Initialize();
+    // 音声再生
+    soundPlayer = new SoundPlayer();
+    soundPlayer->Initialize(soundLoader);
+
+    // SRVマネージャーの初期化
+    srvManager = new SrvManager();
+    srvManager->Initialize(dxCommon);
+    // ImGuiマネージャの初期化
+    imGuiManager = new ImGuiManager();
+    imGuiManager->Initialize(winApp, dxCommon, srvManager);
+    // テクスチャマネージャーの初期化
+    TextureManager::GetInstance()->Initialize(dxCommon, srvManager);
+    // 3Dモデルマネージャの初期化
+    ModelManager::GetInstance()->Initialize(dxCommon);
+
+    TextureManager::GetInstance()->LoadTexture("Resources/uvChecker.png");
+    TextureManager::GetInstance()->LoadTexture("Resources/monsterBall.png");
+    TexturePath01 = "Resources/uvChecker.png";
+    TexturePath02 = "Resources/monsterBall.png";
+
+    // .objファイルからモデルを読み込む
+    ModelManager::GetInstance()->LoadTexture("plane.obj");
+    ModelManager::GetInstance()->LoadTexture("axis.obj");
+    ModelPath01 = "plane.obj";
+    ModelPath02 = "axis.obj";
+
+    // 音声ファイル
+    soundData = soundLoader->SoundLoadWave("Resources/Alarm01.wav");
+
+    // 汎用機能の初期化 
+
+    // 入力の初期化
+    input = new Input();
+    input->Initialize(winApp);
+
+#pragma region 基盤システムの初期化
+
+    // スプライト共通部の初期化
+    spriteCommon = new SpriteCommon;
+    spriteCommon->Initialize(dxCommon);
+
+    // 3Dオブジェクト共通部の初期化
+    object3dCommon = new Object3dCommon;
+    object3dCommon->Initialize(dxCommon);
+
+    // カメラの初期化
+    camera = new Camera();
+    camera->SetRotate({ 0.0f,0.0f,0.0f });
+    camera->SetTranslate({ 0.0f,0.0f,-700.0f });
+    object3dCommon->SetDefaultCamera(camera);
+
+#pragma endregion 基盤システムの初期化
+
+#pragma region 最初のシーンの初期化
+
+    // スプライトの初期化
+    sprite = new Sprite;
+    sprite->Initialize(spriteCommon);
+    sprite->Crrate(TexturePath01, { 100.0f,100.0f }, 0.0f, { 360.0f,360.0f });
+
+    // 3Dモデルの初期化
+    model = new Model;;
+    model->Initialize(ModelManager::GetInstance()->GetModelCommon(), "Resources", ModelPath01);
+
+    // 3Dオブジェクトの初期化
+    object3d = new Object3d;
+    object3d->Initialize(object3dCommon);
+    // オブジェクト作成
+    object3d->Crrate(ModelPath01, { { 1.0f, 1.0f, 1.0f }, { 0.0f, 3.0f, 0.0f }, { 0.0f, -0.5f, 0.0f } });
+
+#pragma endregion 最初のシーンの初期化
+
+    //リソースリークチェック
+    D3DResourceLeakChecker leakCheck;
+
+    // カメラの現在の位置と回転を取得
+    Cameraposition = camera->GetTranslate();
+    Camerarotation = camera->GetRotate();
+
+    // 音声プレイフラグ
+    soundfige = 0;
 }
 
 void MyGame::Update() {
--- project/MyGame.h
+++ project/MyGame.h
@@ -1,21 +1,69 @@
 #pragma once
+#include<MatrixVector.h>
+#include<ResourceObject.h>
+#include<Input.h>
+#include<DirectXCommon.h>
+#include<D3DResourceLeakChecker.h>
+#include<Transform.h>
+#include<TextureManager.h>
+#include<SpriteCommon.h>
+#include<Sprite.h>
+#include<Object3dCommon.h>
+#include<Object3d.h>
+#include<ModelCommon.h>
+#include<Model.h>
+#include<ModelManager.h>
+#include<Camera.h>
+#include<SrvManager.h>
+#include<ImGuiManager.h>
+#include<SoundPlayer.h>
 
 // ゲーム
 class MyGame
 {
 public: // メンバ関数
 
-	// 初期化
-	void Initialize();
-	// 終了
-	void Finalize();
-	// 毎フレーム更新
-	void Update();
-	// 描画
-	void Draw();
+    // 初期化
+    void Initialize();
+    // 終了
+    void Finalize();
+    // 毎フレーム更新
+    void Update();
+    // 描画
+    void Draw();
 
 private: // メンバ変数
+    // ポインタ
+    Input* input = nullptr;
+    WinApp* winApp = nullptr;
+    DirectXCommon* dxCommon = nullptr;
+    SpriteCommon* spriteCommon = nullptr;
+    Object3dCommon* object3dCommon = nullptr;
+    ModelCommon* modelCommon = nullptr;
+    SrvManager* srvManager = nullptr;
+    ImGuiManager* imGuiManager = nullptr;
+    SoundLoader* soundLoader = nullptr;
+    SoundPlayer* soundPlayer = nullptr;
 
 
+    Camera* camera;
+    Sprite* sprite;
+    Model* model;
+    Object3d* object3d;
 
+    // カメラの現在の位置と回転を取得
+    Vector3 Cameraposition;
+    Vector3 Camerarotation;
+
+    // 音声プレイフラグ
+    uint32_t soundfige;
+
+
+    SoundData soundData;
+
+    std::string TexturePath01;
+    std::string TexturePath02;
+
+    std::string ModelPath01;
+    std::string ModelPath02;
 };
