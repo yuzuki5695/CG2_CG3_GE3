4777cfb6e67d9926b0ba7f3fa988afc100c35047
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-02-17 15:48:48 +0900

    GE3_11_01_完了

---
+++ project/MyGame.cpp
@@ -0,0 +1,17 @@
+#include "MyGame.h"
+
+void MyGame::Finalize() {
+
+}
+
+void MyGame::Initialize() {
+
+}
+
+void MyGame::Update() {
+
+}
+
+void MyGame::Draw() {
+
+}
---
+++ project/MyGame.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// ゲーム
+class MyGame
+{
+public: // メンバ関数
+
+	// 初期化
+	void Initialize();
+	// 終了
+	void Finalize();
+	// 毎フレーム更新
+	void Update();
+	// 描画
+	void Draw();
+
+private: // メンバ変数
+
+
+
+};
