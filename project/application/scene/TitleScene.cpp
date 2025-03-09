3790e965917f259d024f9fc2ff677d50499bdfa8
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-03-09 23:17:32 +0900

    GE3_12_04_完了

--- project/application/scene/TitleScene.cpp
+++ project/application/scene/TitleScene.cpp
@@ -49,7 +49,7 @@ void TitleScene::Update() {
 
 
     if (Input::GetInstance()->Triggrkey(DIK_SPACE) && soundfige == 0) {
-       // soundfige = 1;
+        soundfige = 1;
     }
 
     if (soundfige == 1) {
