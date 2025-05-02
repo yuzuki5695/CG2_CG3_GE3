a6cdb40bf04b29dd70c778f1f863ee9229a81a25
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-05-02 11:00:32 +0900

    02_00_導入

--- project/Resources/shaders/Object3d.PS.hlsl
+++ project/Resources/shaders/Object3d.PS.hlsl
@@ -5,6 +5,7 @@ struct Material
     float4 color;
     int endbleLighting;
     float32_t4x4 uvTransform;
+    float shininess;
 };
 
 struct DirectionalLight
@@ -14,8 +15,14 @@ struct DirectionalLight
     float intensity; //!< 輝度
 };
 
+struct Camera
+{
+    float3 worldPosition;
+};
+
 ConstantBuffer<Material> gMaterial : register(b0);
 ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+ConstantBuffer<Camera> gCamera : register(b2);
 
 struct PixeShaderOutput
 {
@@ -28,12 +35,12 @@ SamplerState gSampler : register(s0);
 
 PixeShaderOutput main(VertexShaderOutput input)
 {
-    PixeShaderOutput output;
-    
     //TextureをSampling
     float4 TransformesUV = mul(float4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
     float4 textureColor = gTexture.Sample(gSampler, TransformesUV.xy);
     
+    PixeShaderOutput output;
+     
     // textureのα値が0.5以下のときにpixelを棄却
     if (textureColor.a <= 0.5)
     {
@@ -57,9 +64,20 @@ PixeShaderOutput main(VertexShaderOutput input)
         // half lambert
         float Ndont = dot(normalize(input.normal), -gDirectionalLight.direction);
         float cos = pow(Ndont * 0.5f + 0.5f, 2.0f);
-         
-        // 色にはLightingを行い,a値には行わないようにする
-        output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        float3 toEve = normalize(gCamera.worldPosition - input.worldPosition);
+        
+        float3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
+        float RdotE = dot(reflectLight, toEve);
+        float specularPow = pow(saturate(RdotE), gMaterial.shininess); // 反射強度
+        
+        // 拡散反射
+        float3 diffuse =
+        gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        // 鏡面反射
+        float3 specular =
+        gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f, 1.0f, 1.0f);
+        // 拡散反射 * 鏡面反射
+        output.color.rgb = diffuse + specular;
         output.color.a = gMaterial.color.a * textureColor.a;
     }
     else
--- project/engine/3d/Object3dCommon.cpp
+++ project/engine/3d/Object3dCommon.cpp
@@ -49,7 +49,7 @@ void Object3dCommon::RootSignatureGenerate() {
     /*----------------------------------------------------------------------------------*/
     /*---------------------------------RootParameter作成---------------------------------*/
     /*----------------------------------------------------------------------------------*/
-    D3D12_ROOT_PARAMETER rootParameters[4] = {};
+    D3D12_ROOT_PARAMETER rootParameters[5] = {};
     rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// CBVを使う
     rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;// PixelShaderで使う
     rootParameters[0].Descriptor.ShaderRegister = 0;// レジスタ番号0を使う
@@ -66,6 +66,10 @@ void Object3dCommon::RootSignatureGenerate() {
     rootParameters[3].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// CBVを使う
     rootParameters[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;// PixelShaderで使う
     rootParameters[3].Descriptor.ShaderRegister = 1;// レジスタ番号1を使う
+
+    rootParameters[4].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// CBVを使う
+    rootParameters[4].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;// PixelShaderで使う
+    rootParameters[4].Descriptor.ShaderRegister = 2;// レジスタ番号2を使う
 
     /*----------------------------------------------------------------------------------*/
     /*---------------------------------RootSignature作成---------------------------------*/
